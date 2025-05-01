bf711569058f591fc070f5ba64eeea41fb417e90
Dinusha Madhushani <madhushaniD2002@gmail.com>
2025-05-01 23:30:19 +0530

    updates

--- auth/login.php
+++ auth/login.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                                 header("Location: ../admins/dashboard.php");
                                 exit();
                             case 'Staff':
-                                header("Location: ../staff/dashboard.php");
+                                header("Location: ../staff/dashboard copy.php");
                                 exit();
                             case 'Customer':
                                 header("Location: ../users/user_dashboard.php");
